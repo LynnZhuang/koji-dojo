4b663ab4d63e444a73db8b0fd471c7a828450c8e
lzhuang <lzhuang@redhat.com>
2018-10-16 14:12:46 +0800

    update schema.sql dir

--- hub/bin/setup_psql.sh
+++ hub/bin/setup_psql.sh
@@ -3,5 +3,5 @@
 set -x
 psql="psql --host=koji-db --username=koji koji"
 
-cat /opt/koji/docs/schema.sql | $psql
+cat /usr/local/src/koji/docs/schema.sql | $psql
 echo "BEGIN WORK; INSERT INTO content_generator(name) VALUES('test-cg'); COMMIT WORK;" | $psql
