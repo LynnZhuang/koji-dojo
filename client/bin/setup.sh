38d13ff20859cc4ab7198cb92e9825915415ee9f
lzhuang <lzhuang@redhat.com>
2018-10-24 17:26:18 +0800

    modify servername

--- client/bin/setup.sh
+++ client/bin/setup.sh
@@ -9,7 +9,7 @@ fi
 
 while true; do
 	echo "Waiting for koji-hub to start..."
-    hubstart=$(curl -X GET http://koji-hub:8080/)
+    hubstart=$(curl -X GET http://koji-hub:8080/koji/)
 	echo $hubstart
 	if [ "x$hubstart" != "x" ]; then
 		echo "koji-hub started:"
