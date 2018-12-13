558d568016cc0441f2716f27f95df402dfd883ee
lzhuang <lzhuang@redhat.com>
2018-12-13 17:21:24 +0800

    change path to .pgpass

--- hub/bin/setup_psql.sh
+++ hub/bin/setup_psql.sh
@@ -2,16 +2,16 @@
 
 set -x
 
-echo "Create /opt/.pgpass"
-cat <<EOF >> /opt/.pgpass
+echo "Create /opt/koji-clients/.pgpass"
+cat <<EOF >> /opt/koji-clients/.pgpass
 koji-db:5432:koji:koji:mypassword
 EOF
-chmod 600 /opt/.pgpass
-ls -la /opt/
+chmod 600 /opt/koji-clients/.pgpass
+ls -la /opt/koji-clients
 
-cat /opt/.pgpass
+cat /opt/koji-clients/.pgpass
 
-psql="PGPASSFILE=/opt/.pgpass psql --host=koji-db --username=koji koji"
+psql="PGPASSFILE=/opt/koji-clients/.pgpass psql --host=koji-db --username=koji koji"
 
 cat /usr/local/src/koji/docs/schema.sql | $psql
 echo "BEGIN WORK; INSERT INTO content_generator(name) VALUES('test-cg'); COMMIT WORK;" | $psql
