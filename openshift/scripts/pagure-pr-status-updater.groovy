5621fc5bf23bb1825536b30ede15f45644360c78
lzhuang <lzhuang@redhat.com>
2019-02-11 11:15:03 +0800

    modify groovy script

--- openshift/scripts/pagure-pr-status-updater.groovy
+++ openshift/scripts/pagure-pr-status-updater.groovy
@@ -1,55 +1,86 @@
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
-import static groovyx.net.http.HttpBuilder.configure
-import static groovyx.net.http.ContentTypes.JSON
-import groovyx.net.http.*
-import static groovy.json.JsonOutput.*
-
-// magic to get jenkins build properties
-def build = this.getProperty('binding').getVariable('build')
-def listener = this.getProperty('binding').getVariable('listener')
-def env = build.getEnvironment(listener)
-println "Environment Variables:"
-println prettyPrint(toJson(env))
-
-def setBuildStatusOnPagurePR(pagure_site, pagure_repo_name, pr_no, token, percent, build_url, String comment) {
-  def pagureUrl = "/api/0/${pagure_repo_name}/pull-request/${pr_no}/flag"
-  def payload = [ 'username': 'c3i-jenkins',
-                'percent' : percent,
-                'comment' : comment,
-                'url'     : build_url,
-                'uid'     : 'ci-pre-merge'
-              ]
-  println "Sending Args:"
-  println payload
-
-  def result = configure {
-    request.uri = pagure_site
-    request.contentType = JSON[0]
-    request.headers['Authorization'] = "token ${token}"
-  }.post {
-    request.uri.path = pagureUrl
-    request.body = payload
-    request.contentType = 'application/x-www-form-urlencoded'
-    request.encoder 'application/x-www-form-urlencoded', NativeHandlers.Encoders.&form
+import java.net.URLEncoder
+class PagureClient {
+  String pagureApiUrl
+  String token
+  def steps
+  def callApi(String httpMode, String apiPath, Map payload = null) {
+    def headers = []
+    if (token) {
+      headers << [name: 'Authorization', value: "token ${token}", maskValue: true]
+    }
+    def payloadItems = []
+    if (payload) {
+      for (it in payload) {
+        if (it == null || it.value == null) {
+          continue
+        }
+        payloadItems << (URLEncoder.encode(it.key.toString(), 'utf-8') +
+          '=' +  URLEncoder.encode(it.value.toString(), 'utf-8'))
+      }
+    }
+    return steps.httpRequest(
+      httpMode: httpMode,
+      url: "${pagureApiUrl}/${apiPath}",
+      acceptType: 'APPLICATION_JSON',
+      contentType: 'APPLICATION_FORM',
+      requestBody: payloadItems.join('&'),
+      customHeaders: headers,
+    )
   }
-
-  println prettyPrint(toJson(result))
+  def getPR(Map args) {
+    def apiPath = "${args.fork?'fork/':''}${args.repo}/pull-request/${args.pr}"
+    def response = callApi('GET', apiPath)
+    return steps.readJSON(text: response.content)
+  }
+  def updatePRStatus(Map args) {
+    def apiPath = "${args.fork?'fork/':''}${args.repo}/pull-request/${args.pr}/flag"
+    def response = callApi('POST', apiPath, [
+      'username': args.username,
+      'uid': args.uid,
+      'percent': args.percent,
+      'comment': args.comment,
+      'url': args.url,
+    ])
+    return steps.readJSON(text: response.content)
+  }
+  def commentOnPR(Map args) {
+    def apiPath = "${args.fork?'fork/':''}${args.repo}/pull-request/${args.pr}/comment"
+    def response = callApi('POST', apiPath, [
+      'comment': args.comment,
+    ])
+    return steps.readJSON(text: response.content)
+  }
 }
-
-def commentOnPR(pagure_site, pagure_repo_name, pr_no, token, String comment) {
-  def pagureUrl = "/api/0/${pagure_repo_name}/pull-request/${pr_no}/comment"
-  def payload = [ 'comment': comment]
-
-  def result = configure {
-    request.uri = pagure_site
-    request.contentType = JSON[0]
-    request.headers['Authorization'] = "token ${token}"
-  }.post {
-    request.uri.path = pagureUrl
-    request.body = payload
-    request.contentType = 'application/x-www-form-urlencoded'
-    request.encoder 'application/x-www-form-urlencoded', NativeHandlers.Encoders.&form
+def getPagurePRInfo() {
+  def pagureClient = new PagureClient (pagureApiUrl: env.PAGURE_API, steps: steps)
+  return pagureClient.getPR(fork: env.PAGURE_REPO_IS_FORK == 'true', repo: "${PAGURE_REPO_NAME}", pr: env.PR_NO)
+}
+def setBuildStatusOnPagurePR(percent, String comment) {
+  withCredentials([string(credentialsId: "${env.PIPELINE_NAMESPACE}-${PAGURE_API_KEY_SECRET_NAME}", variable: 'TOKEN')]) {
+    def pagureClient = new PagureClient (pagureApiUrl: env.PAGURE_API, token: env.TOKEN, steps: steps)
+    pagureClient.updatePRStatus(
+      username: 'c3i-jenkins', uid: 'ci-pre-merge', url: env.DEV_BUILD_URL?:env.BUILD_URL,
+      percent: percent, comment: comment, pr: env.PR_NO,
+      repo: "${PAGURE_REPO_NAME}", fork: env.PAGURE_REPO_IS_FORK == 'true')
   }
-
-  println prettyPrint(toJson(result))
+}
+def commentOnPR(String comment) {
+  withCredentials([string(credentialsId: "${env.PIPELINE_NAMESPACE}-${PAGURE_API_KEY_SECRET_NAME}", variable: 'TOKEN')]) {
+    def pagureClient = new PagureClient (pagureApiUrl: env.PAGURE_API, token: env.TOKEN, steps: steps)
+    pagureClient.commentOnPR(
+      comment: comment, pr: env.PR_NO,
+      repo: "${PAGURE_REPO_NAME}", fork: env.PAGURE_REPO_IS_FORK == 'true')
+  }
+}
+def sendBuildStatusEmail(boolean success) {
+  def status = success ? 'passed' : 'failed'
+  def reciepent = env.PAGURE_POLLING_FOR_PR != 'true' && ownership.job.ownershipEnabled && ownership.job.primaryOwnerEmail ?
+    ownership.job.primaryOwnerEmail : env.GIT_AUTHOR_EMAIL
+  def subject = "Jenkins job ${env.JOB_NAME} #${env.BUILD_NUMBER} ${status}."
+  def body = "Build URL: ${env.DEV_BUILD_URL}"
+  if (env.PAGURE_POLLING_FOR_PR == 'true') {
+    subject = "Jenkins job ${env.JOB_NAME}, PR #${env.PR_NO} ${status}."
+    body += "\nPull Request: ${env.PR_URL}"
+  }
+  emailext to: reciepent, subject: subject, body: body
 }
